e7ab853eccfbf4557175f10db50ea10313054fa0
Ahtisham8952 <ahtisham.badar786@gmail.com>
2024-05-09 21:21:26 +0500

    conversions in js

--- conversion.js
+++ conversion.js
@@ -1,12 +1,15 @@
-let number= 82
-let stringNumber= String(number)
-console.log( typeof stringNumber);
+// let number= 82
+// let stringNumber= String(number)
+// console.log( typeof stringNumber);
 
 
-let string="ahtisham"
-let NumberName=Number(string)
-console.log(NumberName);
-console.log(typeof NumberName);
+// let string="ahtisham"
+// let NumberName=Number(string)
+// console.log(NumberName);
+// console.log(typeof NumberName);
 
 // true => 1
 // false =>0
+let num=33;
+let Convert= String(num)
+console.log(typeof Convert )
