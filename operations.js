82bfc5a3c629c493ba907eb21316af61f1289fcb
Ahtisham8952 <ahtisham.badar786@gmail.com>
2024-05-16 21:08:32 +0500

    practice

---
+++ operations.js
@@ -0,0 +1,52 @@
+"use strict"
+// const str = 1234567;
+// const reversedStr = Number(str.toString().split("").reverse().join(""));
+// console.log(reversedStr);
+// const num = 12345657;
+// const reversedNum = num.toString().split("").reverse().join("");
+// console.log(typeof reversedNum);
+// const myarray=[1,2,3,4]
+// console.log(`this is original array ${myarray}`)
+// const secondarray=[...myarray]
+// console.log(`this is second array ${secondarray}`)
+// const myColor = ["Red", "Green", "White", "Black"];
+// console.log(myColor.toString())
+// console.log(myColor.join('+'))
+// console.log(myColor[2])
+//  function arrayeven(num) {
+//     if (num%2==0) {
+//         arrayeven.push('-',2)
+//     } else {
+//         console.log("error")
+//     }
+
+    
+// }
+// arrayeven()
+// var data = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+// for (let x = 0; x < data.length; x++) {
+//     // console.log(x)
+//     for (let y = 0; y < data.length; y++) {
+//         console.log(data)
+//         if (data[y]>data[y+1]) {
+//             let temp=data[y]
+//             data[y]=data[y+1]
+//             data[y+1]=temp
+           
+            
+//         }
+        
+//     }
+    
+// }
+// console.log(data)
+// let s=""
+// const array=[1,2,3,4,5,6,7,8]
+// const secondarry=array.join()
+// console.log(` secondry array ${secondarry}`)
+//  s += secondarry;
+// console.log(s)
+const array=[1,2,3,4,5,6,7,8]
+for(const i of array){
+co
+};
